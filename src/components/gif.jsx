c8a829c9702399c0e43145381967843c374dff83
Robert King <robert.o.king@hotmail.co.uk>
2020-11-05 16:02:50 +0100

    Started: App, SearchBar and Gif components

---
+++ src/components/gif.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  render() {
+    return (
+      <image src="{src}" className="gif"></image>
+    );
+  }
+};
+
+export default Gif;
---
+++ src/components/search_bar.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+/*class SearchBar extends Component {
+  render() {
+    return (
+      <input type="text" className="form-control form-search" />
+    );
+  }
+};*/
+
+const SearchBar = () => {
+  return (
+    <input type="text" className="form-control form-search" />
+  );
+};
+
+export default SearchBar;
